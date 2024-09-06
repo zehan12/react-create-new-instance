37577a962dfe489f78a6edf06a8e3ad1751b7aee
zehan12 <zehan9211@gmail.com>
2024-09-06 19:08:29 +0530

    fix build issue

--- src/components/Input.tsx
+++ src/components/Input.tsx
@@ -1,3 +1,13 @@
-export const Input = ({ props }: any) => {
-    return <input {...props} className=" bg-gray-900 p-2 my-2" type="text" />;
+type InputProps = {
+    placeholder: string;
+};
+
+export const Input = ({ placeholder }: InputProps) => {
+    return (
+        <input
+            placeholder={placeholder}
+            className=" bg-gray-900 p-2 my-2"
+            type="text"
+        />
+    );
 };
